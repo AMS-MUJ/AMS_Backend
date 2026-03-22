c045e6e5956f9721e7106e4e8e3550daa2e2befc
Siddhant Sharma <siddhant4040s@gmail.com>
2026-03-22 11:40:35 +0530

    Rename router variable to Adminrouter

--- router/admin.routes.js
+++ router/admin.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { migrateSubmissionsToUsers,autoCreateCommonSection } from "../controller/migration.controller.js";
 
-const router = express.Router();
+const Adminrouter = express.Router();
 
 // PROTECT THIS ROUTE! (Use your verifyJWT or Admin middleware)
 router.post("/run-migration", migrateSubmissionsToUsers);
